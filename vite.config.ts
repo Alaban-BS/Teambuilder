65282d293e3c4e05264534b6651ae9ee399892c9
Antjan Laban <alaban@bs-gg.com>
2025-05-14 11:31:08 +0000

    fix: update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Teambuilder/',
+  base: process.env.NODE_ENV === 'production' ? '/Teambuilder/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 3000,
