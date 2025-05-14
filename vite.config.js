3ffc1b88aeb305c8baf0a87e3c1dbd8f0a171f5b
Antjan Laban <alaban@bs-gg.com>
2025-05-14 08:37:20 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Bind to all network interfaces
     allowedHosts: ['127.0.0.1', '9zsp6-34455.csb.app'], // Allow only these hosts
-    port: 34455, // Use a different port if 5173 is unavailable
+    port: 3000, 
   },
 });
